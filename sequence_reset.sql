df6551ccda38c1498bc41771b49a2c4a68286847
Anton Schmidt <schmidt.anton2@gmail.com>
2019-11-29 14:18:25 +0100

    Sequences & Triggers
    Functionality:
    Adds Auto-Increment of Primary-Keys for all current tables.
    
    How to use:
    Execute after initial table creation, Sequences -> Triggers.
    
    Problems:
    Currently not executable as one file

---
+++ sequence_reset.sql
@@ -0,0 +1,19 @@
+-- Sequences for autoincrementing IDs
+
+DROP SEQUENCE sq_person;
+DROP SEQUENCE sq_personal;
+DROP SEQUENCE sq_student;
+DROP SEQUENCE sq_studiengang;
+DROP SEQUENCE sq_lva;
+DROP SEQUENCE sq_lektor_lva;
+DROP SEQUENCE sq_lva_einheit;
+
+CREATE SEQUENCE sq_person;
+CREATE SEQUENCE sq_personal;
+CREATE SEQUENCE sq_student;
+CREATE SEQUENCE sq_studiengang;
+CREATE SEQUENCE sq_lva;
+CREATE SEQUENCE sq_lektor_lva;
+CREATE SEQUENCE sq_lva_einheit;
+
+
