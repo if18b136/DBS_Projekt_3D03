f79f0956bd6c966868185a6223d2b12350761a70
Cuauhtechalotl <49694317+Cuauhtechalotl@users.noreply.github.com>
2020-01-13 12:43:03 +0100

    update person

--- functions/create_person.sql
+++ functions/create_person.sql
@@ -1,3 +1,11 @@
+/*********************************************************************
+/**
+/** Procedure: create_person
+/** In: f_vorname, f_nachname, f_geburtsdatum - Name and day of birth of a person
+/** Description: Inserts a new entry into table person
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE create_person (f_vorname IN VARCHAR2, f_nachname IN VARCHAR2, f_geburtsdatum IN VARCHAR2)
 AS
 BEGIN
