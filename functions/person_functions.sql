deb3175ade61ac74954fc961942acfbdc341583a
Cuauhtechalotl <49694317+Cuauhtechalotl@users.noreply.github.com>
2020-01-13 13:31:23 +0100

    update header

--- functions/person_functions.sql
+++ functions/person_functions.sql
@@ -1,3 +1,11 @@
+/*********************************************************************
+/**
+/** Procedure: sp_add_rank
+/** In: f_person_id, f_rang - ID of a person, rank
+/** Description: Adds a new entry to the table personenrang
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE sp_add_rank (f_person_id IN NUMBER, f_rang IN NUMBER)
 AS
 BEGIN
@@ -5,6 +13,14 @@ BEGIN
 	VALUES (f_person_id, f_rang);
 END;
 /
+
+/*********************************************************************
+/**
+/** Procedure: sp_remove_rank
+/** In: f_person_id, f_rang - ID of a person, rank
+/** Description: Removes an entry from the table personenrang
+/**
+/*********************************************************************
 
 CREATE OR REPLACE PROCEDURE sp_remove_rank (f_person_id IN NUMBER, f_rang IN NUMBER)
 AS
@@ -14,6 +30,14 @@ BEGIN
 END;
 /
 
+/*********************************************************************
+/**
+/** Procedure: sp_add_staff
+/** In: f_person_id, f_rang - ID of a person, wage-level, username, password
+/** Description: Adds a new entry to the table personal
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE sp_add_staff (f_person_id IN NUMBER, f_gehaltsklasse IN VARCHAR2, f_username IN VARCHAR2, f_password IN VARCHAR2)
 AS
 BEGIN
@@ -21,6 +45,14 @@ BEGIN
 	VALUES (f_person_id, f_gehaltsklasse, f_username, f_password);
 END;
 /
+
+/*********************************************************************
+/**
+/** Procedure: sp_delete_staff
+/** In: f_personal_id - ID of staff
+/** Description: Removes an entry from the table personal
+/**
+/*********************************************************************
 
 CREATE OR REPLACE PROCEDURE sp_delete_staff (f_personal_id IN NUMBER)
 AS
@@ -30,6 +62,14 @@ BEGIN
 END;
 /
 
+/*********************************************************************
+/**
+/** Procedure: sp_add_student
+/** In: f_person_id - ID of a person
+/** Description: Adds a new entry to the table student
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE sp_add_student (f_person_id IN NUMBER)
 AS
 BEGIN
@@ -37,6 +77,14 @@ BEGIN
 	VALUES (f_person_id);
 END;
 /
+
+/*********************************************************************
+/**
+/** Procedure: sp_delete_student
+/** In: f_person_id - ID of a person
+/** Description: Removes an entry from the table student
+/**
+/*********************************************************************
 
 CREATE OR REPLACE PROCEDURE sp_delete_student (f_student_id IN NUMBER)
 AS
@@ -46,6 +94,14 @@ BEGIN
 END;
 /
 
+/*********************************************************************
+/**
+/** Procedure: sp_add_account
+/** In: f_student_id, f_studiengang_id, username, password - ID of the student, ID of a degree course, username and password of the account
+/** Description: Adds a new entry to the table account
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE sp_add_account (f_student_id IN NUMBER, f_studiengang_id IN NUMBER, f_username IN VARCHAR2, f_password VARCHAR2)
 AS
 BEGIN
@@ -53,6 +109,14 @@ BEGIN
 	VALUES (f_student_id, f_studiengang_id, f_username, f_password);
 END;
 /
+
+/*********************************************************************
+/**
+/** Procedure: sp_delete_student
+/** In: f_username - Username of the student's account
+/** Description: Removes an entry from the table account
+/**
+/*********************************************************************
 
 CREATE OR REPLACE PROCEDURE sp_delete_account (f_username IN VARCHAR2)
 AS
@@ -62,6 +126,14 @@ BEGIN
 END;
 /
 
+/*********************************************************************
+/**
+/** Procedure: sp_add_lector_to_lecture
+/** In: f_personal_id, f_lva_id - ID of a lector, ID of a lecture
+/** Description: Adds a new entry to the table leitet
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE sp_add_lector_to_lecture (f_personal_id IN NUMBER, f_lva_id IN NUMBER)
 AS
 BEGIN
@@ -70,6 +142,14 @@ BEGIN
 END;
 /
 
+/*********************************************************************
+/**
+/** Procedure: sp_remove_lector_from_lecture
+/** In: f_personal_id, f_lva_id - ID of a lector, ID of a lecture
+/** Description: Removes an entry from the table leitet
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE sp_remove_lector_from_lecture (f_personal_id IN NUMBER, f_lva_id IN NUMBER)
 AS
 BEGIN
@@ -77,6 +157,14 @@ BEGIN
 	WHERE ((f_personal_id = fk_personal_id) AND (f_lva_id = fk_lva_id));
 END;
 /
+
+/*********************************************************************
+/**
+/** Procedure: sp_set_head_of_section
+/** In: f_personal_id, f_studiengang_id - ID of the head of section, ID of the section
+/** Description: Sets a new head of section to the given degree course
+/**
+/*********************************************************************
 
 CREATE OR REPLACE PROCEDURE sp_set_head_of_section (f_personal_id IN NUMBER, f_studiengang_id IN NUMBER)
 AS
