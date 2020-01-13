2f1b69f804b2c45792b14bcfcb1758e174e22608
Cuauhtechalotl <49694317+Cuauhtechalotl@users.noreply.github.com>
2020-01-13 13:37:48 +0100

    update header

--- functions/studiengang.sql
+++ functions/studiengang.sql
@@ -1,3 +1,11 @@
+/*********************************************************************
+/**
+/** Procedure: sp_add_major
+/** In: f_uid, f_bezeichnung, f_jahrgang - ID of head of section, Decription of degree course, academic year
+/** Description: Adds a new entry to the table studiengang
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE SP_add_major (f_uid IN NUMBER, f_bezeichnung IN VARCHAR2, f_jahrgang IN VARCHAR2)
 AS
 f_leiter_id NUMBER := '0';
@@ -9,6 +17,13 @@ BEGIN
 END;
 /
 
+/*********************************************************************
+/**
+/** Procedure: sp_delete_major
+/** In: f_studiengang
+/** Description: Removes an entry from the table studiengang
+/**
+/*********************************************************************
 
 CREATE OR REPLACE PROCEDURE sp_delete_major (f_studiengang_id IN NUMBER)
 AS
