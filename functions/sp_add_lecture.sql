d9c70944222810e0d27ca6f0e5b14dd183f9c7ef
Cuauhtechalotl <49694317+Cuauhtechalotl@users.noreply.github.com>
2020-01-13 13:35:40 +0100

    update header

--- functions/sp_add_lecture.sql
+++ functions/sp_add_lecture.sql
@@ -1,3 +1,11 @@
+/*********************************************************************
+/**
+/** Procedure: sp_add_lecture
+/** In: f_studiengang_id, f_titel, f_lektor_id - ID of degree course, titel of lecture, ID of lector
+/** Description: Adds a new entry to the table lva and leitet
+/**
+/*********************************************************************
+
 CREATE OR REPLACE PROCEDURE sp_add_lecture (f_studiengang_id IN NUMBER, f_titel IN VARCHAR2, f_lektor_id In NUMBER)
 AS
 BEGIN
